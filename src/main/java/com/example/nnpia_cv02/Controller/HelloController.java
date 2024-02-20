cd6d8e6eeefcf305faf079c85254e2e931139ff3
SkopovaEva <st72711@upce.cz>
2024-02-20 15:07:15 +0100

    init

--- src/main/java/com/example/nnpia_cv02/Controller/HelloController.java
+++ src/main/java/com/example/nnpia_cv02/Controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.example.nnpia_cv02.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -14,5 +12,13 @@ public class HelloController
     @GetMapping("")
     public String helloWorld() {
         return "Hello world from Spring Boot application.";
+    }
+
+    @GetMapping("/say_hello_to_me/{name}")
+    public String helloSomebody(@PathVariable String name) { return "Hello " + name;}
+
+    @RequestMapping(value = "/hi/{name}", method = RequestMethod.GET)
+    public String getSomeGreetings(@PathVariable String name) {
+        return "Hi " + name;
     }
 }
